b585d08e7a3cbcec05b959eb5f5d6f5f0b9cdd37
Dean Wright <dmwspace@yahoo.com>
2019-12-10 20:48:53 -0800

    expanded word list and tried to add Start Over button

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let url = 'https://api.myjson.com/bins/tt5lk'
+let url = 'https://api.myjson.com/bins/7muh8'
 
 fetch(url)
 
@@ -17,41 +17,51 @@ let englishWord = ''
 let esperantoWord = ''
 let numCorrect = 0
 let numAttempted = 0
-const englishWordCardArea = document.querySelector('.english-word-card')
+let randomArr = []
+let englishWordCardArea = document.querySelector('.english-word-card')
 let submitButton = document.querySelector('#submit-button')
 let resultAreaTop = document.querySelector('#result-line-top')
 let resultAreaBottom = document.querySelector('#result-line-bottom')
 let inputTextBox = document.querySelector('.esperanto-word')
 let startOverButton = document.querySelector('#start-over-button')
+let finalScoreLine = document.querySelector('#final-score-line')
 let inputText = ''
 let currentWord = []
 
 function createWordList(data){
+    for (let i = data.length - 1; i > 0; i--){
+        const j = Math.floor(Math.random() * (i + 1))
+        randomArr.push(data[j])
+    }
     for (let item = 0; item < 10; item++) {
-    wordList.push(data[item])
+    wordList.push(randomArr[item])
     }
     inputTextBox.focus()
     inputTextBox.select
+    englishWordCardArea.innerText = wordList[currentWordIndex].English
 // call esperanto function
-    createWordPair()
+    // createWordPair()
 }
 // create the english word, esperanto word pair (currently on line 51-54)
-function createWordPair() {
-    console.log(wordList)
-    //wordList[previousWordIndex] = ''
-    console.log('this is current word index', currentWordIndex)
-    // console.log(wordList[previousWordIndex])
-    // console.log(wordList[currentWordIndex])
-    //wordList[currentWordIndex] = currentWord[previousWordIndex]
-    englishWordCardArea.innerText = wordList[currentWordIndex].English 
-    esperantoWord = wordList[currentWordIndex].Esperanto
-}
+// function createWordPair() {
+//     console.log(wordList)
+//     //wordList[previousWordIndex] = ''
+    
+//     // console.log(wordList[previousWordIndex])
+//     // console.log(wordList[currentWordIndex])
+//     //wordList[currentWordIndex] = currentWord[previousWordIndex]
+//     englishWordCardArea.innerText = wordList[currentWordIndex].English
+//     esperantoWord = wordList[currentWordIndex].Esperanto
+//     }
 submitButton.addEventListener('click', checkForMatch)
 
-function checkForMatch(evt){  
+function checkForMatch(evt){ 
 
+    console.log('this is current word index', currentWordIndex)
+        
+    esperantoWord = wordList[currentWordIndex].Esperanto
     inputText = inputTextBox.innerText
-  
+
     if (inputText === esperantoWord) {
         numCorrect += 1
         numAttempted += 1
@@ -70,23 +80,24 @@ function checkForMatch(evt){
     inputTextBox.innerText = ''
    // previousWordIndex = currentWordIndex
     currentWordIndex += 1
-    // if (currentWordIndex === 10){
-
-    // } else {
-    createWordPair()
-    // }    
-    //checkForMatch()
-}
-
-      
-        //console.log(englishWord, item)
-        
-        
-       // console.log(esperantoWord, item)
-        
-//function checkForMatch(inputText, esperantoWord) {
     
- 
+    if (currentWordIndex === 10){
+        if (numCorrect >= 8){
+            finalScoreLine.innerText = 'Well done! You got ' + numCorrect + ' out of 10 right. Click the \'Start Over\' button to try a new word list.'
+            startOverButton.addEventListener('click', createWordList())
+            currentWordIndex = 0
+
+        } else {
+            finalScoreLine.innerText = "You got " + numCorrect + ' out of 10 right. You need to get at least 8 out of 10 to move on to a new word list. Click the \'Start Over\' button to try again with the same list.'
+            currentWordIndex = 0
+        
+        startOverButton.addEventListener('click', checkForMatch())
+        
+        }
+     } else {
+        englishWordCardArea.innerText = wordList[currentWordIndex].English
+    }
+    } 
 
 createWordList()
 
